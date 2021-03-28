c6685f04942cb53f9e0c70567d6738ec1d9c06ab
Musty260 <51714409+Musty260@users.noreply.github.com>
2021-03-28 16:14:55 +0100

    About page

--- frontend/src/components/pages/about/About.tsx
+++ frontend/src/components/pages/about/About.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/esm/Button";
 
 import { cx } from "emotion";
 import Styles from "./AboutStyles";
@@ -34,18 +35,57 @@ export default class About extends React.Component<Props, State> {
         return (
             <Template>
                 <CommonLayout>
-                    <Container fluid>
+                    <Container>
                         {/* Header Row (1) */}
                         <Row>
                             <Col>
                                 <h1>About</h1>
                             </Col>
                         </Row>
+                        <Row>
+                            <Col>
+                                <br/>
+                                <h2 style={{ color:"#666" }}>What is Mockchain?</h2>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <p>
+                                    The act of teaching is an incredibly valuable one, with experts passing on years of their specialist experience to students.<br/>
+                                </p>
+                                <div style={{ }}>
+                                    <p>
+                                        <span style={{ fontSize:"16px", textDecoration:"underline"}}>Here at Mockchain, we believe that <span style={{ fontWeight:"bold" }}>those who teach</span> should be <span style={{ fontWeight:"bold" }}>properly compensated</span> for their work.<br/></span>
+                                    </p>
+                                </div>
+                                <p>
+                                    In order to serve these lessons, teachers can host <span style={{ fontWeight:"bold" }}>livestreams</span> on Mockchain.
+                                </p>
+                                <p>
+                                    <h3>Upon visiting mockcha.in:</h3>
+                                    You must either create an account, or login.<br/>
+                                    Upon joining Mockchain for the first time, you will be rewarded with a small amount of MTC token.
+                                    <br/><br/>
+                                    <h3>As a student:</h3>
+                                    Browse Mockchain. Look for a lesson or workshop on a topic planned that interests you. There will be a small entry fee, but upon paying this, you will now have access to that lesson when it goes live.<br/>
+                                    The good part is, your entry fee will be directly payed towards the teacher of the lesson.<br/>
+                                    Thes
+                                    <br/>
+                                    <h3>As a teacher:</h3>
+                                    As a teacher, you have knowledge to share, but you want compensation for this effort. To host on Mockchain, press "Host an Event" at the top. Fill in the details of your planned lesson, as well as an entry fee you feel is fair
+                                    to the effort put into your lesson, and simply submit.<br/>
+                                    You now have access to a live streaming room, where willing students will pay you MTC to join. Here, you will then present the lesson.
+                                </p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Link to="/" style={{ display:"table", marginLeft:"auto", marginRight:"auto" }}>
+                                <Button variant="light">Go Home</Button>
+                            </Link>
+                        </Row>
                     </Container>
                 </CommonLayout>
-                <Link  to="/" hidden>
-                    <span id="go-home">Go Home</span>
-                </Link>
+                
             </Template>
         )
     }
